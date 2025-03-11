035810898357d488b8d1b5bc34f59090451833e4
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-03-11 21:49:43 +0600

    code-crafters-team-project-github-setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Footer from './shared/Footer/Footer'
+import Header from './shared/Header/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Code Crafters team project</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
